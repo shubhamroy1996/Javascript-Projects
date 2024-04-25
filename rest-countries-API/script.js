9148baad27191dbb5ae12b96972c57be5942cc0d
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2024-04-25 22:29:13 +0530

    add: add countries details using dom

--- rest-countries-API/script.js
+++ rest-countries-API/script.js
@@ -1,3 +1,28 @@
+const countriesContainer = document.querySelector('.countries-container')
 fetch("https://restcountries.com/v3.1/all")
-  .then((res) => res.json())
-  .then(console.log);
+  .then((resolve) => resolve.json())
+  .then((data) => {
+    data.forEach((country) => {
+      const countryCard = document.createElement("a");
+      countryCard.classList.add("country-card");
+      const cardHTML = `
+      <img src="https://flagcdn.com/is.svg" alt="flag" />
+      <div class="card-text">
+      <h3 class="card-title">Iceland</h3>
+      <p><b>Population: </b>81,770,900</p>
+      <p><b>Region: </b>Europe</p>
+      <p><b>Capital: </b>Berlin</p>
+      </div>
+      `
+      countryCard.innerHTML = cardHTML
+      countriesContainer.append(countryCard)
+    })
+  })
+
+
+
+
+
+/*   const cardImg = document.createElement('img')
+  cardImg.src = "https://flagcdn.com/is.svg"
+  countryCard.append(cardImg) */
